d9001b2196a689759294ac6f67e37cb3c584a1e8
mabbbeeenh <106855662+mabbbeeenh@users.noreply.github.com>
2022-12-01 03:07:15 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -29,15 +29,12 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '阿雅', year: '2002', date: '11-25',
+          type: '生日', name: '阿雅', year: '2002', date: '11-25',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],
