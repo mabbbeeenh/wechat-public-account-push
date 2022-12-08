22c1c5551ce2652bafb0983b37d2051126f37fd0
mabbbeeenh <106855662+mabbbeeenh@users.noreply.github.com>
2022-12-08 15:56:51 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,15 +21,15 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '阿雅',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oP6X15qHdpqjoIGDMgxwRSrOWwpE',
+      id: 'oP6X15hZTJgBIecG3X-7atfKPbQU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'vAFBoEpvcvqb8lBMS28wQ-4EYkLj6-fS6296p-myaP0',
+      useTemplateId: 'iwUGnZkGpDwm-S-Rj2SqBIBeNfLI2oksg19MbULhQSc',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '7-24',
+      horoscopeDate: '01-08',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '生日', name: '小熊', year: '2002', date: '07-24',
+          type: '生日', name: '阿雅', year: '2002', date: '12-24',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
