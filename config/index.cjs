2d4fca4072b9b3b6c5bf44f32122670d1c66ed1d
mabbbeeenh <106855662+mabbbeeenh@users.noreply.github.com>
2022-12-29 12:49:00 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,7 +13,7 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '264651cb641932d6a5589adda9219359',
 
-  PROVINCE: '贵州',
+  PROVINCE: '黔西南',
   CITY: '兴仁',
 
   USERS: [
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '阿雅',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oP6X15hZTJgBIecG3X-7atfKPbQU',
+      id: 'oP6X15j-twGymE7Rj-wEGJE2zzx4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'AecQ8uxkDXp7fClZETcHVMm9-wkberpGiy7NiDSLPUA',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
