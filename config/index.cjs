c5c61b2bd2ffd3740b04fbaf4e1512504056fa7d
mabbbeeenh <106855662+mabbbeeenh@users.noreply.github.com>
2023-01-20 00:35:26 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oP6X15hZTJgBIecG3X-7atfKPbQU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'Ngm7RRZietHEq_pUlVD3sZ9QwAyeD4OQyOOUj9sANHQ',
+      useTemplateId: 'aplgga9BQrV8W4rkbo-j4RWBXquOSuojrD7f8md5dg4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-08',
       festivals: [
