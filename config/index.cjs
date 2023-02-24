5eb7e8ed028b812cb9a7ced42e9d33c1138c74b1
mabbbeeenh <106855662+mabbbeeenh@users.noreply.github.com>
2023-02-24 13:35:26 +0800

    Create index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '阿雅',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oP6X15hZTJgBIecG3X-7atfKPbQU',
+      id: 'oP6X15j-twGymE7Rj-wEGJE2zzx4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'AWZKwfpQp_vSnLEihEAhhdI22AIfso46BRahYJVXMt0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
