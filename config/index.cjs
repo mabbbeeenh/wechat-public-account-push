a8fa65e81f279ab138b73be39b02a595ca7f3e13
mabbbeeenh <106855662+mabbbeeenh@users.noreply.github.com>
2023-12-01 01:12:56 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -5,7 +5,7 @@
  * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
  * 自行添加属性，以支持更多个性化功能
  */
-export const USER_CONFIG = {
+const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
   APP_ID: 'wx855a6ecffeb5fb0c',
@@ -23,7 +23,7 @@ export const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小琳',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oP6X15j-twGymE7Rj-wEGJE2zzx4',
+      id: 'oP6X15kAs8NfpORIkwDhmH57S1mE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'GUVW11h4pbR3oggRGcBOLZZ6aDLc-KGOLvma-UiW7BY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
